122e405533bd5062b417cdb4cda99fb27c660476
安 <394378627@qq.com>
2018-04-21 21:23:46 +0800

    no message

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -28,23 +28,11 @@ class GameController extends Controller
     #获取身份信息
     public function GameAuth(Request $request)
     {
-//        $userinfo['openid'] = str_random(16);
-//        $userinfo['active_id'] = $request->active->id;
-//        $gamer = User::where('openid', $userinfo['openid'])->where('active_id', $request->active->id)->first();
-//        if (!$gamer) {
-//            $gamer = User::add($userinfo);
-//        }
-//
-//        session(['an_game' => $gamer]);
-//        return redirect('game/' . $request->module);
-//        $account = Account::find($request->active->account_id)->toArray();
         $account['appid'] = 'wxf50ad054693ef907';
         $account['appsecret'] = '10932473244fcc0c4e10afbdcd391d39';
         $wxAuth = new GetAuth($account['appid'], $account['appsecret']);
-        if (!$request->has('code')) {
-            $url = route('game_auth', ['a' => $request->active->id, 'm' => $request->module]);
-            return $wxAuth->getCode($url);
-        } else {
+        if ($request->has('code')) {
+            dd($request->code);
             $user_info = $wxAuth->getUserInfo($request->code);
             $gamer = User::where('openid', $user_info['openid'])->where('active_id', $request->active->id)->first();
             if (!$gamer) {
@@ -52,7 +40,22 @@ class GameController extends Controller
             }
             session('an_game', $gamer);
             return redirect('game/' . $request->module);
+        } else {
+            dd($request->all());
         }
+
+//        if (!$request->has('code')) {
+//            $url = route('game_auth', ['a' => $request->active->id, 'm' => $request->module]);
+//            return $wxAuth->getCode($url);
+//        } else {
+//            $user_info = $wxAuth->getUserInfo($request->code);
+//            $gamer = User::where('openid', $user_info['openid'])->where('active_id', $request->active->id)->first();
+//            if (!$gamer) {
+//                $gamer = User::add($user_info);
+//            }
+//            session('an_game', $gamer);
+//            return redirect('game/' . $request->module);
+//        }
     }
 
 }
--- app/Http/Middleware/WxAuth.php
+++ app/Http/Middleware/WxAuth.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Extension\WxSdk\GetAuth;
 use Closure;
 
 class WxAuth
@@ -17,7 +18,12 @@ class WxAuth
     {
         $gamer=session('an_game');
         if(!$gamer['openid']){
-            return redirect('game/auth');
+            $account['appid'] = 'wxf50ad054693ef907';
+            $account['appsecret'] = '10932473244fcc0c4e10afbdcd391d39';
+            $wxAuth = new GetAuth($account['appid'], $account['appsecret']);
+            $url = route('game_auth', ['a' => $request->active->id, 'm' => $request->module]);
+            return $wxAuth->getCode($url);
+//            return redirect('game/auth');
         }
         $request->offsetSet('gamer', $gamer);
         unset($gamer);
