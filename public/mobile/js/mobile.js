dee0841197b82384254f74a551baf7019fe87874
yy2034 <yangy@fengrongwang.com>
2018-04-23 17:46:28 +0800

    修改js判断

--- public/mobile/js/mobile.js
+++ public/mobile/js/mobile.js
@@ -2,9 +2,9 @@ $(function(){
 	var h = $(window).height();
 	var h2 = $('.actIndex').height();
 	if(h2<h){
-		$('.actIndex').css({position:'fixed',width:'100%',height:'100%'});
+		$('.actIndex').css({position:'fixed',width:'100%',height:'100%',overflow:'hidden'});
 	}else{
-		$('.actIndex').css({position:'relative',height:'auto'})
+		$('.actIndex').removeAttr('width').css({position:'relative',height:'auto',overflow:'hidden'})
 	}
 })
 
