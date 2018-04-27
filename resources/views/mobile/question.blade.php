a7bb9cc696dfb861d5d1ca864f73c77965748a1a
安智超 <394378627@qq.com>
2018-04-27 10:56:08 +0800

    coding

--- resources/views/mobile/question.blade.php
+++ resources/views/mobile/question.blade.php
@@ -45,7 +45,7 @@
             </div>
         </div> -->
         <!-- 等待下一题开发弹出层 -->
-        <div class="waitLoad1" id="wait_question" style="display:none;">
+        <div class="waitLoad1" id="wait_question" >
             <div class="loading">
                 <div class="load">
                     <div class="loadMain">
@@ -59,6 +59,7 @@
     </div>
 </div>
 <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="../mobile/js/mobile.js"></script>
 
 <!-- 答题正确弹框 -->
 <div class="boxShadow boxShadow1" style="display: none">
@@ -111,9 +112,93 @@
     var connected=false;
     var game_status = 0;
     var left_timer;
-    var user_id = "{{$user->id}}";
-    var active_id = "{{$active->id}}";
+    var user_id = "88";
+    var active_id = "1";
+    {{--var user_id = "{{$user->id}}";--}}
+    {{--var active_id = "{{$active->id}}";--}}
     var question_id = 0;
+
+
+    var i = 0;
+    var j = 0;
+    var count = 0;
+    var MM = 0;
+    var SS = 15;  // 秒 90s
+    var MS = 0;
+    var totle = (MM+1)*600;
+    var d = 180*(MM+1);
+
+    var gameTime =15;
+    //count down
+    var showTime = function(){
+        totle = totle - 1;
+        if (totle == 0) {
+            clearInterval(s);
+            clearInterval(t1);
+            $(".pie2").css("-o-transform", "rotate(" + d + "deg)");
+            $(".pie2").css("-moz-transform", "rotate(" + d + "deg)");
+            $(".pie2").css("-webkit-transform", "rotate(" + d + "deg)");
+        } else {
+            if (totle > 0 && MS > 0) {
+                MS = MS - 1;
+                if (MS < 10) {
+                    MS =MS
+                };
+            };
+            if (MS == 0 && SS > 0) {
+                MS = 10;
+                SS = SS - 1;
+                if (SS < 10) {
+                    SS =  SS
+                };
+            };
+            if (SS == 0 && MM > 0) {
+                SS = 60;
+                MM = MM - 1;
+                if (MM < 10) {
+                    MM =  MM
+                };
+            };
+        };
+        $(".time").html(SS + "s");
+    };
+
+    var start1 = function(){
+        //i = i + 0.6;
+        i = i + 360/((gameTime)*10);  //旋转的角度  90s 为 0.4  60s为0.6
+        console.log(i);
+        count = count + 1;
+        if(count <= (gameTime/2*10)){  // 一半的角度  90s 为 450
+            $(".pie1").css("-o-transform","rotate(" + i + "deg)");
+            $(".pie1").css("-moz-transform","rotate(" + i + "deg)");
+            $(".pie1").css("-webkit-transform","rotate(" + i + "deg)");
+        }else{
+            $(".pie2").css("backgroundColor", "#d13c36");
+            $(".pie2").css("-o-transform","rotate(" + i + "deg)");
+            $(".pie2").css("-moz-transform","rotate(" + i + "deg)");
+            $(".pie2").css("-webkit-transform","rotate(" + i + "deg)");
+        }
+    };
+
+    var countDown = function() {
+        //80*80px 时间进度条
+        i = 0;
+        j = 0;
+        count = 0;
+        MM = 0;
+        SS = gameTime;
+        MS = 0;
+        totle = (MM + 1) * gameTime * 10;
+        d = 180 * (MM + 1);
+        MM = "0" + MM;
+        // showTime();
+        s = setInterval("showTime()", 100);
+        // start1();
+        //start2();
+        t1 = setInterval("start1()", 100);
+    }
+
+
     var websocket = new WebSocket(wsServer);
 
     window.onload = function () {
@@ -170,85 +255,6 @@
                     });
 
                     $("#questions").show();
-                    i = 0;
-                    j = 0;
-                    count = 0;
-                    MM = 0;
-                    SS = 10;  // 秒 90s
-                    MS = 0;
-                    totle = (MM+1)*600;
-                    d = 180*(MM+1);
-                    MM = "0" + MM;
-                    var gameTime = 10;
-                    //count down
-                    var showTime = function(){
-                        totle = totle - 1;
-                        if (totle == 0) {
-                            clearInterval(s);
-                            clearInterval(t1);
-                            clearInterval(t2);
-                            $(".pie2").css("-o-transform", "rotate(" + d + "deg)");
-                            $(".pie2").css("-moz-transform", "rotate(" + d + "deg)");
-                            $(".pie2").css("-webkit-transform", "rotate(" + d + "deg)");
-                        } else {
-                            if (totle > 0 && MS > 0) {
-                                MS = MS - 1;
-                                if (MS < 10) {
-                                    MS =MS
-                                };
-                            };
-                            if (MS == 0 && SS > 0) {
-                                MS = 10;
-                                SS = SS - 1;
-                                if (SS < 10) {
-                                    SS =  SS
-                                };
-                            };
-                            if (SS == 0 && MM > 0) {
-                                SS = 60;
-                                MM = MM - 1;
-                                if (MM < 10) {
-                                    MM =  MM
-                                };
-                            };
-                        };
-                        $(".time").html(SS + "s");
-                    };
-
-                    var start1 = function(){
-                        //i = i + 0.6;
-                        i = i + 360/((gameTime)*10);  //旋转的角度  90s 为 0.4  60s为0.6
-                        console.log(i);
-                        count = count + 1;
-                        if(count <= (gameTime/2*10)){  // 一半的角度  90s 为 450
-                            $(".pie1").css("-o-transform","rotate(" + i + "deg)");
-                            $(".pie1").css("-moz-transform","rotate(" + i + "deg)");
-                            $(".pie1").css("-webkit-transform","rotate(" + i + "deg)");
-                            }else{
-                            $(".pie2").css("backgroundColor", "#d13c36");
-                            $(".pie2").css("-o-transform","rotate(" + i + "deg)");
-                            $(".pie2").css("-moz-transform","rotate(" + i + "deg)");
-                            $(".pie2").css("-webkit-transform","rotate(" + i + "deg)");
-                        }
-                    };
-
-                    var countDown = function() {
-                        //80*80px 时间进度条
-                        i = 0;
-                        j = 0;
-                        count = 0;
-                        MM = 0;
-                        SS = gameTime;
-                        MS = 0;
-                        totle = (MM + 1) * gameTime * 10;
-                        d = 180 * (MM + 1);
-                        MM = "0" + MM;
-                        // showTime();
-                        s = setInterval("showTime()", 100);
-                        // start1();
-                        //start2();
-                        t1 = setInterval("start1()", 100);
-                    }
                     countDown();
                     start1();
                     $("#questions").hide();
