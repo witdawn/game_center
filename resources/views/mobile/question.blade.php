7ce4a90c902f8f6eb22fc6b0fe457273283366f7
安智超 <394378627@qq.com>
2018-04-27 15:33:50 +0800

    no message

--- resources/views/mobile/question.blade.php
+++ resources/views/mobile/question.blade.php
@@ -20,12 +20,12 @@
             <div class="quesCir">
                 <div class="game_time">
                     <div class="hold">
-                    <div class="pie pie1" style="transform: rotate(156.6deg);"></div>
+                        <div class="pie pie1" style="transform: rotate(156.6deg);"></div>
                     </div>
                     <div class="hold">
-                    <div class="pie pie2"></div>
+                        <div class="pie pie2"></div>
                     </div>
-                    <div class="bg"> </div>
+                    <div class="bg"></div>
                     <div class="quesCir1 time" id="left_seccond"></div>
                 </div>
             </div>
@@ -36,16 +36,16 @@
             </div>
         </div>
         <!-- 耐心等待题目开放 -->
-<!--         <div class="waitLoad2" id="wait_question">
-            <div class="loading">
-                <div class="load">
-                    <a><img src="../mobile/imgs/bao1.png" alt=""></a>
-                    <p id="wait_title">请耐心等待题目开放……</p>
-                </div>
-            </div>
-        </div> -->
+        <!--         <div class="waitLoad2" id="wait_question">
+                    <div class="loading">
+                        <div class="load">
+                            <a><img src="../mobile/imgs/bao1.png" alt=""></a>
+                            <p id="wait_title">请耐心等待题目开放……</p>
+                        </div>
+                    </div>
+                </div> -->
         <!-- 等待下一题开发弹出层 -->
-        <div class="waitLoad1" id="wait_question" style="display: none" >
+        <div class="waitLoad1" id="wait_question" style="display: none">
             <div class="loading">
                 <div class="load">
                     <div class="loadMain">
@@ -109,99 +109,87 @@
 </div>
 <script>
     var wsServer = 'ws://my.witdawn.com:9501/';
-    var connected=false;
+    var connected = false;
     var game_status = 0;
-    var left_timer;
-    var left_timer1;
     var user_id = "88";
     var active_id = "1";
-    {{--var user_id = "{{$user->id}}";--}}
-    {{--var active_id = "{{$active->id}}";--}}
+            {{--var user_id = "{{$user->id}}";--}}
+            {{--var active_id = "{{$active->id}}";--}}
     var question_id = 0;
 
-
     var i = 0;
-    var j = 0;
     var count = 0;
-    var MM = 0;
     var SS = 15;  // 秒 90s
     var MS = 0;
-    var totle = (MM+1)*600;
-    var d = 180*(MM+1);
+    var totle = 600;
+    var d = 180;
+    var left_timer;
+    var left_timer1;
 
-    var gameTime =15;
-    //count down
-    var showTime = function(){
-        totle = totle - 1;
-        if (totle == 0) {
-            clearInterval(left_timer);
-            clearInterval(left_timer1);
-            $(".pie2").css("-o-transform", "rotate(" + d + "deg)");
-            $(".pie2").css("-moz-transform", "rotate(" + d + "deg)");
-            $(".pie2").css("-webkit-transform", "rotate(" + d + "deg)");
-        } else {
-            if (totle > 0 && MS > 0) {
-                MS = MS - 1;
-                if (MS < 10) {
-                    MS =MS
-                };
-            };
-            if (MS == 0 && SS > 0) {
-                MS = 10;
-                SS = SS - 1;
-                if (SS < 10) {
-                    SS =  SS
-                };
-            };
-            if (SS == 0 && MM > 0) {
-                SS = 60;
-                MM = MM - 1;
-                if (MM < 10) {
-                    MM =  MM
-                };
-            };
-        };
-        $(".time").html(SS + "s");
-    };
-
-    var start1 = function(){
-        //i = i + 0.6;
-        i = i + 360/((gameTime)*10);  //旋转的角度  90s 为 0.4  60s为0.6
-        console.log(i);
-        count = count + 1;
-        if(count <= (gameTime/2*10)){  // 一半的角度  90s 为 450
-            $(".pie1").css("backgroundColor", "#21B5F2");
-            $(".pie1").css("-o-transform","rotate(" + i + "deg)");
-            $(".pie1").css("-moz-transform","rotate(" + i + "deg)");
-            $(".pie1").css("-webkit-transform","rotate(" + i + "deg)");
-        }else{
-            $(".pie2").css("backgroundColor", "#21B5F2");
-            $(".pie2").css("-o-transform","rotate(" + i + "deg)");
-            $(".pie2").css("-moz-transform","rotate(" + i + "deg)");
-            $(".pie2").css("-webkit-transform","rotate(" + i + "deg)");
-        }
-    };
-
-    var countDown = function() {
-        //80*80px 时间进度条
-        i = 0;
-        j = 0;
-        count = 0;
-        MM = 0;
-        SS = gameTime;
-        MS = 0;
-        totle = (MM + 1) * gameTime * 10;
-        d = 180 * (MM + 1);
-        MM = "0" + MM;
-        left_timer = setInterval("showTime()", 100);
-        left_timer1 = setInterval("start1()", 100);
-    }
+    var gameTime = 15;
 
 
     var websocket = new WebSocket(wsServer);
 
     window.onload = function () {
 
+        var showTime = function () {
+            totle = totle - 1;
+            if (totle === 0) {
+                clearInterval(left_timer);
+                clearInterval(left_timer1);
+                $(".pie2").css("-o-transform", "rotate(" + d + "deg)");
+                $(".pie2").css("-moz-transform", "rotate(" + d + "deg)");
+                $(".pie2").css("-webkit-transform", "rotate(" + d + "deg)");
+                sendAnswer(0);
+                $("#questions").hide();
+            } else {
+                if (totle > 0 && MS > 0) {
+                    MS = MS - 1;
+                    if (MS < 10) {
+                        MS = MS
+                    }
+                    ;
+                }
+                ;
+                if (MS == 0 && SS > 0) {
+                    MS = 10;
+                    SS --;
+                    if (SS < 10) {
+                        SS = SS
+                    };
+                }
+            }
+            $(".time").html(SS + "s");
+        };
+
+        var start1 = function () {
+            i = i + 360 / ((gameTime) * 10);  //旋转的角度  90s 为 0.4  60s为0.6
+            count = count + 1;
+            if (count <= (gameTime / 2 * 10)) {  // 一半的角度  90s 为 450
+                $(".pie1").css("backgroundColor", "#21B5F2");
+                $(".pie1").css("-o-transform", "rotate(" + i + "deg)");
+                $(".pie1").css("-moz-transform", "rotate(" + i + "deg)");
+                $(".pie1").css("-webkit-transform", "rotate(" + i + "deg)");
+            } else {
+                $(".pie2").css("backgroundColor", "#21B5F2");
+                $(".pie2").css("-o-transform", "rotate(" + i + "deg)");
+                $(".pie2").css("-moz-transform", "rotate(" + i + "deg)");
+                $(".pie2").css("-webkit-transform", "rotate(" + i + "deg)");
+            }
+        };
+
+        var countDown = function () {
+            i = 0;
+            count = 0;
+            SS = gameTime;
+            MS = 0;
+            totle = gameTime * 10;
+            d = 180;
+            left_timer = setInterval(showTime(), 100);
+            start1();
+            left_timer1 = setInterval(start1(), 100);
+        }
 
         websocket.onopen = function (evt) {
             websocket.send(JSON.stringify({
@@ -213,7 +201,7 @@
             }));
             $("#loading").hide();
             $("#wait_question").show();
-            connected=true;
+            connected = true;
         };
 
         websocket.onclose = function (evt) {
@@ -258,7 +246,6 @@
 
                     $("#questions").show();
                     countDown();
-                    start1();
                     // var left_time = 10;
                     // left_timer = setInterval(function () {
                     //     console.log(left_time);
@@ -307,11 +294,11 @@
             }));
         }
 
-        setTimeout(function(){
-            if(!connected){
+        setTimeout(function () {
+            if (!connected) {
                 window.location.reload()
             }
-        },5000);
+        }, 5000);
     };
     //关闭页面时 退出登录
     $(window).unload(function () {
