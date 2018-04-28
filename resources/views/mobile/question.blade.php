40b5e3622f6f0cd2bd9f85a18fcf6a3b8fc4540f
yy2034 <yangy@fengrongwang.com>
2018-04-28 10:13:25 +0800

    cod

--- resources/views/mobile/question.blade.php
+++ resources/views/mobile/question.blade.php
@@ -53,7 +53,7 @@
             <div class="loading">
                 <div class="load">
                     <div class="loadMain">
-                        <img style="width:50%;margin-bottom:20px;" src="../mobile/imgs/gif.gif" alt="">
+                        <img style="width:50%;margin-bottom:20px;" src="../mobile/imgs/gif2.gif" alt="">
                         <a>请耐心等待</a>
                         <a>题目即将开放</a>
                     </div>
